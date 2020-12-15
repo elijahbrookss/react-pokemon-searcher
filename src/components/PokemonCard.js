278efacf5d24aecbb07c10d3db0dd83b8a72a921
elijahcbrooks <elijahcbrooks@gmail.com>
2020-12-15 15:27:41 -0600

    finished project

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,47 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  constructor(props){
+    super(props);
+    const { id, name, hp, sprites } = props.pokemon
+    const { front, back } = sprites;
+    this.id = id
+    this.name = name
+    this.hp = hp
+    this.front = front
+    this.back = back
+
+    this.state = {
+      image: this.front
+    }
+  }
+
+  rotateImage = (e) => {
+    let img = e.target;
+    let front = this.front;
+    let back = this.back;
+    this.setState({
+      image: img.src === front ? back : front
+    });
+
+  }
+
   render() {
+
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img onClick={this.rotateImage} src={this.state.image} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,47 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {pokemon: []}
+  renderPokemon = []
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pokemon")
+    .then(response=>response.json())
+    .then(pokemon => {
+      this.renderPokemon = pokemon
+      this.setState({ pokemon })
+    })
+  }
+
+  searchPokemon = event => {
+    const input = event.target.value;
+    const pokemon = this.renderPokemon;
+
+    const pokemonResults = pokemon.filter(pokeman => {
+      if(pokeman.name.includes(input)){
+        return pokeman
+      }
+    })
+
+    this.setState({ pokemon: pokemonResults })
+  }
+
+  createPokemon = pokemon => {
+    this.renderPokemon = [...this.renderPokemon, pokemon];
+    this.setState({pokemon: this.renderPokemon})
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm createPokemon={this.createPokemon} />
         <br />
-        <Search />
+        <Search searchPokemon={this.searchPokemon} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} />
       </Container>
     )
   }
